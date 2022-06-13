10a48b78c300ab119ed63276e0f236d393100c7e
MosbyTheGreat <moritz.larndorfer@gmail.com>
2022-06-13 21:45:41 +0200

    chessboard for camera calibration added

--- Camera Calibration/test_calibration.py
+++ Camera Calibration/test_calibration.py
@@ -1,12 +1,13 @@
 import os
 import time
+import timeit
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
 # values
-camera_matrix = np.array([[1.52244073e+03,  0.00000000e+00, 1.18803566e+03]
-                          [0.00000000e+00,  1.50426034e+03, 7.84669829e+02]
+camera_matrix = np.array([[1.52244073e+03,  0.00000000e+00, 1.18803566e+03],
+                          [0.00000000e+00,  1.50426034e+03, 7.84669829e+02],
                           [0.00000000e+00,  0.00000000e+00, 1.00000000e+00]])
 dist_coefs = np.array([-0.31086836,  0.10644732,  0.0030072,  -0.0012158,  -0.01891462])
 
@@ -22,11 +23,15 @@ images = [ cv2.imread('./Images/' + fn.__str__(),cv2.IMREAD_GRAYSCALE) for fn in
 images = [ x for x in images if x is not None ] # filter empty images
 
 for img in images:
-    # remove the the lens distortions
+    start = timeit.default_timer()
+    # remove the the lens distortion
     dst = cv2.undistort(img, camera_matrix, dist_coefs, None, camera_matrix)
 
+    stop = timeit.default_timer()
+
     # show image
-    cv2.imshow("AMRAS Viewfinder", dst)
+    cv2.imshow("undistorted image", dst)
+
     cv2.waitKey(1)
     plt.show()
     time.sleep(1)
